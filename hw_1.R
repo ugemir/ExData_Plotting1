947af5bf1a2bd93674bfc823d9403792d3472765
unknown <ozatau@mef.edu.tr>
2020-06-03 18:11:33 +0300

    added graphs

--- hw_1.R
+++ hw_1.R
@@ -20,4 +20,27 @@ data$newdatetime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:
 
 subdata <- subset(data,newdatetime >= "2007-02-01" & newdatetime <= "2007-02-02")
 
-subdata
+subdata$Global_active_power2 <- as.numeric(subdata$Global_active_power)
+
+hist(subdata$Global_active_power2)
+
+plot(subdata$newdatetime,subdata$Global_active_power,type = "l")
+
+with(subdata,{
+
+plot(subdata$newdatetime,subdata$Sub_metering_1,type = "l")
+
+plot(subdata$newdatetime,subdata$Sub_metering_2,type = "l")
+
+plot(subdata$newdatetime,subdata$Sub_metering_3,type = "l")
+
+              })
+
+
+plot(subdata$newdatetime,subdata$Sub_metering_1,type = "n")
+
+points(subdata$newdatetime,subdata$Sub_metering_1,type = "l")
+
+points(subdata$newdatetime,subdata$Sub_metering_2,type = "l",col = "blue")
+
+points(subdata$newdatetime,subdata$Sub_metering_3,type = "l")
